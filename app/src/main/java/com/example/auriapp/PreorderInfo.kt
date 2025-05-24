16e1aa6c93bf1e60e9416a14e0c9264ba6dedd46
AureumBox <aurimart.liliana@gmail.com>
2025-05-24 19:47:53 -0400

    app icon changes

--- app/src/main/java/com/example/auriapp/PreorderInfo.kt
+++ app/src/main/java/com/example/auriapp/PreorderInfo.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.net.toUri
 
 class PreorderInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +24,8 @@ class PreorderInfo : AppCompatActivity() {
         val btnShop = findViewById<Button>(R.id.btn_shop)
 
         btnShop.setOnClickListener{
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://yenpress.com/titles/9798400903526-omniscient-reader-s-viewpoint-novel-vol-1"))
+            val intent = Intent(Intent.ACTION_VIEW,
+                "https://yenpress.com/titles/9798400903526-omniscient-reader-s-viewpoint-novel-vol-1".toUri())
             startActivity(intent)
         }
     }
