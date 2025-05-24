6dd2adb99fb1079fef59b002298a6f9bbd701d32
AureumBox <aurimart.liliana@gmail.com>
2025-05-24 19:52:26 -0400

    fix

--- app/src/main/java/com/example/auriapp/PreorderInfo.kt
+++ app/src/main/java/com/example/auriapp/PreorderInfo.kt
@@ -1,7 +1,6 @@
 package com.example.auriapp
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
